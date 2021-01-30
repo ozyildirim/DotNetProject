93e3d0ef4f5a83db7eefd2469524344c0e3e8aac
Kutay Yıldırım <kutay9622@gmail.com>
2021-01-31 01:04:13 +0300

    7.gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.inMemory;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,6 +8,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductManager productManager = new ProductManager(new inMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity ı uygulayan eden sınıflar, birer veritabanı tablosudur.
+    interface IEntity
+    {
+
+    }
+}
